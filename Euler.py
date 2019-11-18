899cfbe7fe3d7d13ec0ee11b33e02f7fa4340c7f
laums25 <56890898+laums25@users.noreply.github.com>
2019-11-17 22:43:50 -0500

    Euler.py

--- Euler.py
+++ Euler.py
@@ -2,14 +2,29 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 	
-plt.figure(1)
-	
+plt.figure(1, figsize=(8,4))
+plt.subplot(1,3,1)
 data = np.loadtxt("14.dat")
-	
 
 plt.plot(data[:,0], data[:,1])
-plt.plot(data[:,1], data[:,2])
+
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.savefig("14.png")
+plt.title('yn(0) vs t')
 
+plt.subplot(1,3,2)
+
+plt.plot(data[:,0], data[:,2])
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('yn(1) vs t')
+
+plt.subplot(1,3,3)
+
+plt.plot(data[:,1], data[:,2])
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('yn(0) vs yn(1)')
+plt.savefig("14.png")
