7b196db0d4f1c18168ff1289df7acdadc7346e5f
laums25 <lm.mercado@uniandes.edu.co>
2019-11-18 04:12:48 +0000

    Runge friccion py

---
+++ Rungefri.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+	
+plt.figure(1, figsize=(8,4))
+plt.subplot(1,3,1)
+data = np.loadtxt("datosfri.dat")
+
+plt.plot(data[:,0], data[:,1])
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('yn(0) vs t')
+
+plt.subplot(1,3,2)
+
+plt.plot(data[:,0], data[:,2])
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('yn(1) vs t')
+
+plt.subplot(1,3,3)
+
+plt.plot(data[:,1], data[:,2])
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('yn(0) vs yn(1)')
+plt.savefig("Rungefri.png")
